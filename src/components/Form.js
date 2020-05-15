f70d8723ed84902df70f99478c7c819c6d6576d4
klmeir <caalmeir@gmail.com>
2020-05-15 15:04:29 -0500

    Add Form Appointments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const Form = () => {
-    return ( 
-        <h1>Desde formulario</h1>
-     );
-}
- 
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la mascota"
+        />
+
+        <label>Fecha Dueño</label>
+        <input 
+            type="date" 
+            name="date" 
+            className="u-full-width"                 
+        />
+
+        <label>Hora</label>
+        <input 
+            type="time" 
+            name="hour" 
+            className="u-full-width"             
+        />
+
+        <label>Síntomas</label>
+        <textarea 
+            className="u-full-width" 
+            name="symptoms"
+        >
+        </textarea>
+
+        <button 
+            type="submit" 
+            className="u-full-width button-primary"
+        >
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
 export default Form;
