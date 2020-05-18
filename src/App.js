38858ecf288b8e6f2e69c6a7a7a7dfa3b8042b0b
klmeir <caalmeir@gmail.com>
2020-05-18 00:29:11 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Form from './components/Form';
 import Appointment from './components/Appointment';
 
 function App() {
 
   const [appointments, setAppointments] = useState([]);
+
+  useEffect( () => {
+    console.log('useEffect');
+  }, [appointments]);
 
   const createAppointment = appointment => {
     setAppointments([
